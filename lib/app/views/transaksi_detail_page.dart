4c2a4b61106d72b5ffcd2f8ae7b5f403bee28d24
sholikin <solikhinimut@gmail.com>
2025-05-18 11:51:24 +0700

    add1

--- lib/app/views/transaksi_detail_page.dart
+++ lib/app/views/transaksi_detail_page.dart
@@ -1,1 +1,10 @@
-stf
+import 'package:flutter/material.dart';
+
+class TransaksiDetailPage extends StatelessWidget {
+  const TransaksiDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
